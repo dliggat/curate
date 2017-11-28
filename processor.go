2d1cd4bed0e25365d48b6f300557cace6c58bdea
Dave Liggat <dave@liggat.org>
2017-11-27 16:47:39 -0800

    Use real Message struct

--- processor.go
+++ processor.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"flag"
 	"errors"
 	"github.com/andyfase/CURdashboard/go/curconvert"
 	"log"
@@ -12,18 +13,14 @@ import (
 	"github.com/aws/aws-sdk-go/service/sqs"
 )
 
-type Message struct {
-	Name string
-	Value int64
-}
 
-func gosqs() (Message, error) {
+func gosqs(queueUrl string) (Message, error) {
 	var m Message
 	sess := session.Must(session.NewSessionWithOptions(session.Options{
 		SharedConfigState: session.SharedConfigEnable,
 	}))
  	svc := sqs.New(sess)
-	qURL := "https://sqs.us-west-2.amazonaws.com/824550351281/cursqs-development-Queue-VF7QJ75CC880"
+	qURL := queueUrl
 
 	result, err := svc.ReceiveMessage(&sqs.ReceiveMessageInput{
 	AttributeNames: []*string{
@@ -52,6 +49,7 @@ func gosqs() (Message, error) {
 		if err != nil {
 			return m, err
 		}
+		fmt.Println(*result.Messages[0].Body)
 
 		fmt.Println("Message Deleted", resultDelete)
 		var m Message
@@ -63,10 +61,35 @@ func gosqs() (Message, error) {
 
 }
 
+func getParams(queueUrl *string, topLevelDestPath *string) error {
+	flag.StringVar(queueUrl, "sqsqueue", "", "SQS URL for processing")
+	flag.StringVar(topLevelDestPath, "destpathprefix", "parquet-cur", "Top level destination path")
+	flag.Parse()
+	return nil
+}
+
+type Message struct {
+	CurReportDescriptor string    `json:"cur_report_descriptor"`
+	SourceBucket string           `json:"source_bucket"`
+	DestinationBucket string      `json:"destination_bucket"`
+        ReportPath string             `json:"report_path"`
+        ReportName string             `json:"report_name"`
+	SourceRoleArn string          `json:"source_role_arn"`
+	SourceExternalId string       `json:"source_external_id"`
+	DestinationRoleArn string     `json:"destination_role_arn"`
+	DestinationExternalId string  `json:"destination_external_id"`
+}
+
 
 func main() {
+	var queueUrl, topLevelDestPath string
+	if err := getParams(&queueUrl, &topLevelDestPath); err != nil {
+		fmt.Println(err)
+		return
+	}
 
-	m, err := gosqs()
+
+	m, err := gosqs(queueUrl)
 	if err != nil {
 		fmt.Println(err)
 		fmt.Println("Nothing to do. Exiting.")
@@ -74,25 +97,33 @@ func main() {
 	}
 
 	fmt.Printf("Result: %+v", m)
-
-	var sourceBucket, destBucket, destPath, reportPath, reportName string //inputDate, sourceRoleArn, sourceExternalID, destRoleArn, destExternalID string
-	sourceBucket = "dliggat-billing"
-	destBucket = "dliggat-gocur"
-	reportPath = "foo/bar/newreport"
-	reportName = "newreport"
+	if len(m.SourceBucket) < 1 {
+		log.Fatalln("Must supply a source bucket")
+	}
+	if len(m.CurReportDescriptor) < 1 {
+		log.Fatalln("Must supply a report descriptor")
+	}
 
 	var start time.Time
 	start = time.Now()
 	end := start.AddDate(0, 1, 0)
 	curDate := start.Format("200601") + "01-" + end.Format("200601") + "01"
-	manifest := reportPath + "/" + curDate + "/" + reportName + "-Manifest.json"
+	manifest := m.ReportPath + "/" + curDate + "/" + m.ReportName + "-Manifest.json"
 
 	fmt.Println(manifest)
-	destPath = "parquet-cur/" + start.Format("200601")
+	destPath := topLevelDestPath + "/" + m.CurReportDescriptor + "/" + start.Format("200601")
 
-	cc := curconvert.NewCurConvert(sourceBucket, manifest, destBucket, destPath)
+	cc := curconvert.NewCurConvert(m.SourceBucket, manifest, m.DestinationBucket, destPath)
+
+	if len(m.SourceRoleArn) > 1 {
+		cc.SetSourceRole(m.SourceRoleArn, m.SourceExternalId)
+	}
+	if len(m.DestinationRoleArn) > 1 {
+		cc.SetSourceRole(m.DestinationRoleArn, m.DestinationExternalId)
+	}
+
 	if err := cc.ConvertCur(); err != nil {
 		log.Fatalln(err)
 	}
-	fmt.Println("CUR conversion completed and available at s3://" + destBucket + "/" + destPath + "/")
+	fmt.Println("CUR conversion completed and available at s3://" + m.DestinationBucket + "/" + destPath + "/")
 }
